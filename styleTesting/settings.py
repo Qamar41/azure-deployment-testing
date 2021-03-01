98dd6039418cc272ae413e873bd2d0ac09c41f1d
Qamar Ashfaq <qamarashfaq41@gmail.com>
2021-03-01 20:31:44 +0500

    debug=False

--- styleTesting/settings.py
+++ styleTesting/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'wm7m477&1c^frd#_1msnbk#^y5%9t7nxnc+$3vem%)qtcs3gld'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
